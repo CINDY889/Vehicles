c3bee5036bcfa56e67a8b621f23b067e93dd723f
unknown <cindyvega1607@gmail.com>
2026-09-10 11:43:32 -0600

    descripción de puntos importantes del proyecto

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 ## Descripción del proyecto
 Este proyecto consiste en el desarrollo de una aplicación web interactiva para explorar un conjunto de datos de anuncios de venta de vehículos. La aplicación utiliza Streamlit para crear una interfaz web y Plotly Express para generar gráficos interactivos. 
 
+## Objetivos
+* Analizar los datos de vehículos usados.
+* Explorar la relación entre el kilometraje y el precio.
+* Visualizar la distribución del kilometraje mediante histogramas.
+* Crear gráficos interactivos para facilitar el análisis.
+* Desarrollar una aplicación web utilizando Streamlit.
 
+## Funcionalidad
+La aplicación permite:
+* Visualizar la distribución del kilometraje de los vehículos.
+* Analizar la relación entre kilometraje y precio mediante un gráfico de dispersión.
+* Interactuar con los gráficos para explorar los datos.
+* Consultar los resultados del análisis de manera visual y sencilla.
+
+## Tecnologías utilizadas
+* Python
+* Pandas
+* Plotly Express
+* Streamlit
+* Jupyter Notebook
+* Git y GitHub 
 
 
--- app.py
+++ app.py
@@ -2,3 +2,62 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+st.header("Análisis de anuncios de vehículos")
+st.write( "Creación de un histograma para el conjunto de datos de anuncios de ventas de vehículos")
+
+# Botón para construir el histograma
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    st.write('Distribución del kilometraje de los vehículos')
+    fig = px.histogram(car_data, x='odometer', title='Distribución del kilometraje')
+    st.plotly_chart(fig, use_container_width=True)
+
+# Botón para construir el gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+if scatter_button:
+    st.write('Relación entre el kilometraje y el precio')
+    st.plotly_chart(
+        px.scatter(car_data, x='odometer', y='price', title='Precio vs. kilometraje'),
+        use_container_width=True) 
+
+
+build_histogram = st.checkbox(
+    'Mostrar histograma'
+)
+
+if build_histogram:
+
+    fig = px.histogram(
+        car_data,
+        x='odometer',
+        title='Distribución del kilometraje'
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
+
+
+build_scatter = st.checkbox(
+    'Mostrar gráfico de dispersión'
+)
+
+if build_scatter:
+
+    fig = px.scatter(
+        car_data,
+        x='odometer',
+        y='price',
+        title='Precio vs. kilometraje'
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
+
